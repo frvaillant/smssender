85b36ae774c32d0324cd478c8a2bdd2d689802c2
François VAILLANT <frvaillant@gmail.com>
2023-01-25 21:22:19 +0100

    add comments

--- src/SmsSender.php
+++ src/SmsSender.php
@@ -9,13 +9,25 @@ use Mailjet\Resources;
 class SmsSender
 {
 
+    /**
+     * @var string
+     */
     private string $token;
+
+    /**
+     * @var Client
+     */
     private Client $client;
+
 
     public function __construct()
     {
     }
 
+    /**
+     * @return $this
+     * creates connection with mailjet SMS API
+     */
     public function connect(): self
     {
         if(!$this->token) {
@@ -25,12 +37,22 @@ class SmsSender
         return $this;
     }
 
+    /**
+     * @param string $token
+     * @return $this
+     * Set mailjet api key to mailjet Client
+     */
     public function setToken(string $token): self
     {
         $this->token = $token;
         return $this;
     }
 
+    /**
+     * @param SmsMessage $message
+     * @return bool|null
+     * Senbd SMS Message
+     */
     public function send(SmsMessage $message): ?bool
     {
         if(!$message->hasError()) {
